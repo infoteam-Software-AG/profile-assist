7cc126d9f7ec6779897c423d38958c569de4e8cc
schatt <christian.schatt@infoteam.de>
2025-09-24 23:03:00 +0200

    correcting POM, adding spotless and reformatting

--- src/main/java/de/infoteam/profile_assist/ProfileAssistApplication.java
+++ src/main/java/de/infoteam/profile_assist/ProfileAssistApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfileAssistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfileAssistApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProfileAssistApplication.class, args);
+  }
 }
