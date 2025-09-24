00fed9a66fd2307d92ee1e2712152d8392d8af69
schatt <christian.schatt@infoteam.de>
2025-09-24 11:29:09 +0200

    Initial Commit

---
+++ src/main/java/de/infoteam/profile_assist/ProfileAssistApplication.java
@@ -0,0 +1,13 @@
+package de.infoteam.profile_assist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileAssistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileAssistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/infoteam/profile_assist/ProfileAssistApplicationTests.java
@@ -0,0 +1,13 @@
+package de.infoteam.profile_assist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileAssistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
