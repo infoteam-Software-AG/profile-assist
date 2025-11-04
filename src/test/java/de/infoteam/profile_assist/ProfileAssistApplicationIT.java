914fb27039d633014f4d0840f9de34f3bfea843c
Mori Petzold <mori.petzold@infoteam.de>
2025-11-04 07:02:39 +0100

    Add missing spotless formating

--- src/test/java/de/infoteam/profile_assist/ProfileAssistApplicationIT.java
+++ src/test/java/de/infoteam/profile_assist/ProfileAssistApplicationIT.java
@@ -1,6 +1,5 @@
 package de.infoteam.profile_assist;
 
-import de.infoteam.profile_assist.TestArchitectureRules.IntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
