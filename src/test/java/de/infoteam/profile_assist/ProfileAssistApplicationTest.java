23868528795411a438edbb743e82f709bc5b0b2f
Mori Petzold <mori.petzold@infoteam.de>
2025-11-04 12:53:58 +0100

    Add UnitTest for main for Test Coverage

---
+++ src/test/java/de/infoteam/profile_assist/ProfileAssistApplicationTest.java
@@ -0,0 +1,10 @@
+package de.infoteam.profile_assist;
+
+import org.junit.jupiter.api.Test;
+
+public class ProfileAssistApplicationTest {
+  @Test
+  public void main() {
+    ProfileAssistApplication.main(new String[] {});
+  }
+}
