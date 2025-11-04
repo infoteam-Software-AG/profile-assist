89b3f9908f666665b29ee6f4c7660d0cb01b12fb
goeppel <roland.goeppel@infoteam.de>
2025-11-04 13:59:06 +0100

    ran spotless:apply

--- src/main/java/de/infoteam/profile_assist/service/markdown/MarkdownService.java
+++ src/main/java/de/infoteam/profile_assist/service/markdown/MarkdownService.java
@@ -3,6 +3,7 @@ package de.infoteam.profile_assist.service.markdown;
 import java.util.Map;
 
 public interface MarkdownService {
-    String renderToHtml(String markdown);
-    Map<String, Object> extractFrontMatter(String markdown);
+  String renderToHtml(String markdown);
+
+  Map<String, Object> extractFrontMatter(String markdown);
 }
