e8b51430118aaf24a1503536505b460c7d5dfc4e
infoteam <infoteam42@infoteam.de>
2025-11-13 08:56:40 +0100

    added parameterized tests and override the constructor so it is guaranteed that we don't return null values

--- src/test/java/de/infoteam/profile_assist/domain/entity/CallForBidsTest.java
+++ src/test/java/de/infoteam/profile_assist/domain/entity/CallForBidsTest.java
@@ -1,38 +1,52 @@
 package de.infoteam.profile_assist.domain.entity;
 
+import static org.assertj.core.api.BDDAssertions.then;
+
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.stream.Stream;
 import jdk.jfr.Description;
-import org.assertj.core.api.BDDAssertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CallForBidsTest {
 
-  @Test
-  @Description("When CallForBids is created everything should be null")
-  void testCallForBidsCreationWithoutValues() {
-    CallForBids callForBids = CallForBids.builder().build();
-
-    BDDAssertions.then(callForBids.id()).isNull();
-    BDDAssertions.then(callForBids.description()).isNull();
-    BDDAssertions.then(callForBids.optionalSkills()).isNull();
-    BDDAssertions.then(callForBids.mandatorySkills()).isNull();
-  }
-
-  @Test
-  @Description("When CallForBids is with values created everything should be not null")
-  void testCallForBidsCreationWithValues() {
+  @ParameterizedTest
+  @MethodSource("provideTestDataForCallForBids")
+  @Description("CallForBids should handle various combinations of null and non-null values")
+  void testCallForBidsWithVariousParameters(
+      UUID id,
+      String description,
+      ArrayList<String> mandatorySkills,
+      ArrayList<String> optionalSkills) {
     CallForBids callForBids =
         CallForBids.builder()
-            .id(UUID.randomUUID())
-            .description("lorem ipsum")
-            .mandatorySkills(new ArrayList<>())
-            .optionalSkills(new ArrayList<>())
+            .id(id)
+            .description(description)
+            .mandatorySkills(mandatorySkills)
+            .optionalSkills(optionalSkills)
             .build();
 
-    BDDAssertions.then(callForBids.id()).isNotNull();
-    BDDAssertions.then(callForBids.description()).isEqualTo("lorem ipsum");
-    BDDAssertions.then(callForBids.optionalSkills().size()).isZero();
-    BDDAssertions.then(callForBids.mandatorySkills().size()).isZero();
+    then(callForBids).isNotNull();
+    then(callForBids.id()).isNotNull();
+    then(callForBids.description()).isNotNull();
+    then(callForBids.optionalSkills()).isNotNull();
+    then(callForBids.mandatorySkills()).isNotNull();
+  }
+
+  private static Stream<Arguments> provideTestDataForCallForBids() {
+    return Stream.of(
+        Arguments.of(null, null, null, null),
+        Arguments.of(UUID.randomUUID(), null, null, null),
+        Arguments.of(UUID.randomUUID(), "Lorem Ipsum", null, null),
+        Arguments.of(UUID.randomUUID(), null, new ArrayList<>(), null),
+        Arguments.of(UUID.randomUUID(), null, null, new ArrayList<>()),
+        Arguments.of(UUID.randomUUID(), "Lorem Ipsum", new ArrayList<>(), null),
+        Arguments.of(UUID.randomUUID(), "Lorem Ipsum", null, new ArrayList<>()),
+        Arguments.of(UUID.randomUUID(), "Lorem Ipsum", new ArrayList<>(), new ArrayList<>()),
+        Arguments.of(null, "Lorem Ipsum", null, null),
+        Arguments.of(null, null, new ArrayList<>(), null),
+        Arguments.of(null, null, null, new ArrayList<>()));
   }
 }
