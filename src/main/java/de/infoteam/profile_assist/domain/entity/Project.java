12ed93e2b7e2ed6ba97fd8fd316bcc14bc322bcc
Thomas Weber <karazon@gmail.com>
2025-11-14 17:23:50 +0100

    formatting issues resolved

--- src/main/java/de/infoteam/profile_assist/domain/entity/Project.java
+++ src/main/java/de/infoteam/profile_assist/domain/entity/Project.java
@@ -5,7 +5,8 @@ import lombok.Builder;
 import lombok.NonNull;
 
 @Builder(toBuilder = true)
-public record Project(@NonNull String name, @NonNull String description, @NonNull List<String> technologies)
+public record Project(
+    @NonNull String name, @NonNull String description, @NonNull List<String> technologies)
     implements BuilderSupport<Project> {
 
   public static class ProjectBuilder implements BuilderSupport.Builder<Project> {}
--- src/test/java/de/infoteam/profile_assist/domain/entity/ProjectTest.java
+++ src/test/java/de/infoteam/profile_assist/domain/entity/ProjectTest.java
@@ -1,12 +1,11 @@
 package de.infoteam.profile_assist.domain.entity;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class ProjectTest {
 
