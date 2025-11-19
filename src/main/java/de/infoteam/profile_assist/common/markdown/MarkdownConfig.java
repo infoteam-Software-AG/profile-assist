0d89c1b750bf7067140de7315309a41a80148c26
nal0R-exe <roland.goeppel@infoteam.de>
2025-11-19 15:06:31 +0100

    Feature/49 markdown mapper for converting markdown strings into ast (#63)
    * added flexmark markdown mapper
    
    * wrote new mapper for markdown to ast and vice versa
    
    * added files for new markdown mapper
    
    * added test for markdown mapper
    
    * added null check in toAst method
    
    * added commonmark to POM
    
    * switched from flexmark to commonmark
    
    * added new Commonmark mapper and new Strong Emphasis case
    
    * fixed common mark adding a new line between list items
    
    * fixed sonarqube related issues

---
+++ src/main/java/de/infoteam/profile_assist/common/markdown/MarkdownConfig.java
@@ -0,0 +1,21 @@
+package de.infoteam.profile_assist.common.markdown;
+
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.markdown.MarkdownRenderer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class MarkdownConfig {
+  // AST output
+  @Bean
+  Parser commonmarkParser() {
+    return Parser.builder().build();
+  }
+
+  // markdown output
+  @Bean
+  MarkdownRenderer commonmarkRenderer() {
+    return MarkdownRenderer.builder().build();
+  }
+}
