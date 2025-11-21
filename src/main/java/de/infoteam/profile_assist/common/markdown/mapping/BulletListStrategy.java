63dcc4a0b3bb3fee3e752cbc6cbc6121677ace44
Mori Petzold <mori.petzold@infoteam.de>
2025-11-21 17:21:33 +0100

    Add BulletListStrategy implementation, remove unused methods in mapper

--- src/main/java/de/infoteam/profile_assist/common/markdown/mapping/BulletListStrategy.java
+++ src/main/java/de/infoteam/profile_assist/common/markdown/mapping/BulletListStrategy.java
@@ -1,17 +1,32 @@
 package de.infoteam.profile_assist.common.markdown.mapping;
 
+import de.infoteam.profile_assist.common.markdown.BulletList;
+import de.infoteam.profile_assist.common.markdown.ListItem;
 import de.infoteam.profile_assist.common.markdown.MarkdownNode;
+import java.util.ArrayList;
+import java.util.List;
 import org.commonmark.node.Node;
 
 public class BulletListStrategy implements MappingStrategy {
 
   @Override
   public MarkdownNode mapToAST(Node node) {
-    return null;
+    List<ListItem> items = new ArrayList<>();
+    for (var child = node.getFirstChild(); child != null; child = child.getNext()) {
+      if (child instanceof org.commonmark.node.ListItem childNode) {
+        items.add((ListItem) getChildStrategy(childNode).mapToAST(childNode));
+      }
+    }
+    return new BulletList(items);
   }
 
   @Override
   public Node mapToCommonMark(MarkdownNode node) {
-    return null;
+    var bulletList = new org.commonmark.node.BulletList();
+    bulletList.setTight(true);
+    for (ListItem child : ((BulletList) node).items()) {
+      bulletList.appendChild(getChildStrategy(child).mapToCommonMark(child));
+    }
+    return bulletList;
   }
 }
