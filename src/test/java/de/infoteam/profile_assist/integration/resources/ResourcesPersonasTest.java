17ed501941f0a5a9e3ebbafbaa9f719feb1cc0c3
fserves <fabian.serves@infoteam.de>
2025-11-24 09:42:10 +0100

    Adopt test resources personas to match the new Persona structure
    Also add test to check if test personas are deserializable.

--- src/test/java/de/infoteam/profile_assist/integration/resources/ResourcesPersonasTest.java
+++ src/test/java/de/infoteam/profile_assist/integration/resources/ResourcesPersonasTest.java
@@ -4,30 +4,33 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import de.infoteam.profile_assist.domain.entity.Persona;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class ResourcesPersonasTest {
+class ResourcesPersonasTest {
 
-  private static final Path PERSONAS_BASE = Paths.get("src", "test", "resources", "personas");
+  private static final Path PERSONAS_BASE_PATH = Paths.get("src", "test", "resources", "personas");
 
   private static final String PERSONA_JSON_FILE = "persona.json";
 
-  @DisplayName(
-      "When persona files are scanned and read, then all persona.json files can be deserialized")
+  @DisplayName("When persona files are scanned and read, then all personas can be deserialized")
   @ParameterizedTest(name = "Case {index}: file={0}")
   @MethodSource("personaJsonFiles")
   void allPersonaJsonFilesMustBeValid(Path personaFile) {
     // Arrange
     ObjectMapper mapper = new ObjectMapper();
+    mapper.registerModule(new JavaTimeModule());
 
     // Act & Assert
     assertThatCode(() -> mapper.readValue(personaFile.toFile(), Persona.class))
@@ -35,9 +38,9 @@ public class ResourcesPersonasTest {
   }
 
   static Stream<Path> personaJsonFiles() throws IOException {
-    assertThat(PERSONAS_BASE).exists();
+    assertThat(PERSONAS_BASE_PATH).exists();
 
-    try (Stream<Path> paths = Files.walk(PERSONAS_BASE)) {
+    try (Stream<Path> paths = Files.walk(PERSONAS_BASE_PATH)) {
       List<Path> files =
           paths.filter(p -> p.getFileName().toString().equals(PERSONA_JSON_FILE)).toList();
 
@@ -45,5 +48,11 @@ public class ResourcesPersonasTest {
 
       return files.stream();
     }
+  }
+
+  @Test
+  void test() {
+    LocalDate localDate = LocalDate.now();
+    System.out.println(localDate);
   }
 }
