2b232ca74790dc7d91ec591a5b0fb4d187cedf1e
goeppel <roland.goeppel@infoteam.de>
2025-11-24 12:02:12 +0100

    split up markdownbuilder into seperate builder files

---
+++ src/main/java/de/infoteam/profile_assist/common/markdown/BulletListBuilder.java
@@ -0,0 +1,47 @@
+package de.infoteam.profile_assist.common.markdown;
+
+import java.util.function.Consumer;
+
+public final class BulletListBuilder {
+
+  private final DocumentBuilder parent;
+  private final BulletList.BulletListBuilder builder = BulletList.builder();
+
+  BulletListBuilder(DocumentBuilder parent) {
+    this.parent = parent;
+  }
+
+  public BulletListBuilder addListItem(String text) {
+    builder.item(
+      ListItem.builder()
+        .child(
+          Paragraph.builder()
+            .child(Text.builder().text(text).build())
+            .build()
+        )
+        .build()
+    );
+    return this;
+  }
+
+  public BulletListBuilder addListItem(Consumer<InlineBuilder> inlineConfig) {
+    InlineBuilder inlineBuilder = new InlineBuilder();
+    inlineConfig.accept(inlineBuilder);
+
+    builder.item(
+      ListItem.builder()
+        .child(
+          Paragraph.builder()
+            .children(inlineBuilder.buildChildren())
+            .build()
+        )
+        .build()
+    );
+    return this;
+  }
+
+  public DocumentBuilder endBulletList() {
+    parent.builder.child(builder.build());
+    return parent;
+  }
+}
--- src/main/java/de/infoteam/profile_assist/common/markdown/MarkdownBuilder.java
+++ src/main/java/de/infoteam/profile_assist/common/markdown/MarkdownBuilder.java
@@ -1,93 +1,10 @@
 package de.infoteam.profile_assist.common.markdown;
 
-import java.util.function.Consumer;
-
 public final class MarkdownBuilder {
+
   private MarkdownBuilder() {}
 
   public static DocumentBuilder document() {
     return new DocumentBuilder();
-  }
-
-  public static final class DocumentBuilder {
-    private final Document.DocumentBuilder builder = Document.builder();
-
-    public DocumentBuilder addParagraph(String text) {
-      builder.child(Paragraph.builder().child(Text.builder().text(text).build()).build());
-      return this;
-    }
-
-    public DocumentBuilder addParagraph(Consumer<InlineBuilder> inlineConfig) {
-      InlineBuilder inlineBuilder = new InlineBuilder();
-      inlineConfig.accept(inlineBuilder);
-
-      builder.child(Paragraph.builder().children(inlineBuilder.buildChildren()).build());
-      return this;
-    }
-
-    public DocumentBuilder addHeading(int level, String text) {
-      builder.child(
-          Heading.builder().level(level).child(Text.builder().text(text).build()).build());
-      return this;
-    }
-
-    public BulletListBuilder addBulletList() {
-      return new BulletListBuilder(this);
-    }
-
-    public Document build() {
-      return builder.build();
-    }
-  }
-
-  public static final class BulletListBuilder {
-    private final DocumentBuilder parent;
-    private final BulletList.BulletListBuilder builder = BulletList.builder();
-
-    BulletListBuilder(DocumentBuilder parent) {
-      this.parent = parent;
-    }
-
-    public BulletListBuilder addListItem(String text) {
-      builder.item(
-          ListItem.builder()
-              .child(Paragraph.builder().child(Text.builder().text(text).build()).build())
-              .build());
-      return this;
-    }
-
-    public BulletListBuilder addListItem(Consumer<InlineBuilder> inlineConfig) {
-      InlineBuilder inlineBuilder = new InlineBuilder();
-      inlineConfig.accept(inlineBuilder);
-
-      builder.item(
-          ListItem.builder()
-              .child(Paragraph.builder().children(inlineBuilder.buildChildren()).build())
-              .build());
-      return this;
-    }
-
-    public DocumentBuilder endBulletList() {
-      parent.builder.child(builder.build());
-      return parent;
-    }
-  }
-
-  public static final class InlineBuilder {
-    private final java.util.List<MarkdownNode> children = new java.util.ArrayList<>();
-
-    public InlineBuilder text(String text) {
-      children.add(Text.builder().text(text).build());
-      return this;
-    }
-
-    public InlineBuilder strong(String text) {
-      children.add(StrongEmphasis.builder().delimiter("**").text(text).build());
-      return this;
-    }
-
-    java.util.List<MarkdownNode> buildChildren() {
-      return java.util.List.copyOf(children);
-    }
   }
 }
