4032897c63a210360ffcbb33a4dbb40317899ab4
koerner <andreas.koerner@infoteam.de>
2026-01-16 12:40:10 +0100

    improved test coverage

--- src/test/java/de/infoteam/profile_assist/common/markdown/MarkdownConfigTest.java
+++ src/test/java/de/infoteam/profile_assist/common/markdown/MarkdownConfigTest.java
@@ -1,11 +1,14 @@
+// SPDX-FileCopyrightText: 2026 infoteam Software AG
+// SPDX-License-Identifier: Apache-2.0
+// For full license text see: https://github.com/infoteam-Software-AG/profile-assist/blob/main/LICENSE
 package de.infoteam.profile_assist.common.markdown;
+
+import static org.junit.Assert.assertNotNull;
 
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.markdown.MarkdownRenderer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MarkdownConfigTest {
 
